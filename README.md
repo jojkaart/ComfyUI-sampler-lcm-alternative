04e26f93077a32c4cfd23ef57e9b1116368f8c39
Joel Kaartinen <jkaartinen@iki.fi>
2023-11-21 04:28:40 +0200

    Add SamplerLCMCycle

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # ComfyUI-sampler-lcm-alternative
-ComfyUI Custom Sampler nodes that add a new improved LCM scheduler function
+ComfyUI Custom Sampler nodes that add a new improved LCM sampler functions
 
-This custom node repository adds two new nodes for ComfyUI to the Custom Sampler category. SamplerLCMAlternative and LCMScheduler (just to save a few clicks, as you could also the BasicScheduler and choose smg_uniform).
+This custom node repository adds three new nodes for ComfyUI to the Custom Sampler category. SamplerLCMAlternative, SamplerLCMCycle and LCMScheduler (just to save a few clicks, as you could also use the BasicScheduler and choose smg_uniform).
 Just clone it into your custom_nodes folder and you can start using it as soon as you restart ComfyUI.
 
 SamplerLCMAlternative has two extra parameters.
-- `euler_steps`, which is an integer and allows you tell the sampler to use Euler sampling for the first n steps (or skip euler only for last n steps when n is negative).
-- `ancestral`, no clue if the way I add randomness actually matches Euler ancestral sampler, but regardless, if you give this a value above 0.0, the Euler steps get some fresh randomness injected each step. The value controls how much.
+- `euler_steps`, which tells the sampler to use Euler sampling for the first n steps (or skip euler only for last n steps if n is negative).
+- `ancestral`, If you give this a value above 0.0, the Euler steps get some fresh randomness injected each step. The value controls how much.
 
 With default parameters, this sampler acts exactly like the original LCM sampler from ComfyUI. When you start tuning, I recommend starting by setting `euler_steps` to half of the total step count this sampler will be handling. going higher will increase details/sharpness and lower will decrease both.
+
+SamplerLCMCycle has three extra parameters. This sampler repeats a cycle of Euler and LCM sampling steps until inference is done.
+If you're doing txt2img with LCM and feel like LCM is giving boring or artificial looking images, give this sampler a try.
+- `euler_steps`, sets the number of euler steps per cycle
+- `lcm_steps`, sets the number of lcm steps per cycle
+- `ancestral`, same as with SamplerLCMAlternative
+
+The default settings should work fine. I recommend using at least 6 steps to allow for 2 full cycles, that said, this sampler seems to really benefit from extra steps.
--- sampler_lcm_alt.py
+++ sampler_lcm_alt.py
@@ -4,31 +4,51 @@ from tqdm.auto import trange, tqdm
 import torch
 
 @torch.no_grad()
-def sample_lcm_alt(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, euler_steps=-3, ancestral=0.0):
+def sample_lcm_alt(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, euler_steps=0, ancestral=0.0, loop_mode=False):
     extra_args = {} if extra_args is None else extra_args
     noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
     s_in = x.new_ones([x.shape[0]])
-    euler_limit = euler_steps%(len(sigmas)-1)
+    steps = len(sigmas)-1
+    euler_limit = euler_steps%steps
+    loop_control = [True] * euler_limit + [False] * (steps - euler_limit)
+    return sample_lcm_backbone(model, x, sigmas, extra_args, callback, disable, noise_sampler, loop_control, ancestral)
+
+@torch.no_grad()
+def sample_lcm_cycle(model, x, sigmas, extra_args=None, callback=None, disable=None, noise_sampler=None, euler_steps = 1, lcm_steps = 1, ancestral=0.0):
+    extra_args = {} if extra_args is None else extra_args
+    noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
+    s_in = x.new_ones([x.shape[0]])
+    steps = len(sigmas) - 1
+    repeats = steps // (euler_steps + lcm_steps)
+    leftover = steps % (euler_steps + lcm_steps)
+    cycle = [True] * euler_steps + [False] * lcm_steps
+    loop_control = cycle[:leftover] + cycle * repeats
+    return sample_lcm_backbone(model, x, sigmas, extra_args, callback, disable, noise_sampler, loop_control, ancestral)
+
+@torch.no_grad()
+def sample_lcm_backbone(model, x, sigmas, extra_args, callback, disable, noise_sampler, loop_control, ancestral):
+    s_in = x.new_ones([x.shape[0]])
     for i in trange(len(sigmas) - 1, disable=disable):
         denoised = model(x, sigmas[i] * s_in, **extra_args)
         if callback is not None:
             callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
 
-        if i < euler_limit:
-            if ancestral < 1.0:
-                removed_noise = (x - denoised) / sigmas[i]
-            if ancestral > 0.0:
-                noise = noise_sampler(sigmas[i], sigmas[i + 1])
+        if sigmas[i + 1] > 0:
+            if loop_control[i]:
                 if ancestral < 1.0:
-                    noise = (ancestral**0.5) * noise + ((1.0 - ancestral)**0.5) * removed_noise
-            elif ancestral == 0.0:
-                noise = removed_noise
-        elif sigmas[i + 1] > 0:
-            noise = noise_sampler(sigmas[i], sigmas[i + 1])
+                    removed_noise = (x - denoised) / sigmas[i]
+                if ancestral > 0.0:
+                    noise = noise_sampler(sigmas[i], sigmas[i + 1])
+                    if ancestral < 1.0:
+                        noise = (ancestral**0.5) * noise + ((1.0 - ancestral)**0.5) * removed_noise
+                elif ancestral == 0.0:
+                    noise = removed_noise
+            else:
+                noise = noise_sampler(sigmas[i], sigmas[i + 1])
         else:
             noise = None
         x = denoised
-        if sigmas[i + 1] > 0 and torch.is_tensor(noise):
+        if noise is not None:
             x += sigmas[i + 1] * noise
     return x
 
@@ -66,8 +86,27 @@ class SamplerLCMAlternative:
         sampler = comfy.samplers.KSAMPLER(sample_lcm_alt, extra_options={"euler_steps": euler_steps, "ancestral": ancestral})
         return (sampler, )
 
+class SamplerLCMCycle:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required":
+                    {"euler_steps": ("INT", {"default": 1, "min": 1, "max": 50}),
+                     "lcm_steps": ("INT", {"default": 2, "min": 1, "max": 50}),
+                     "ancestral": ("FLOAT", {"default": 0, "min": 0, "max": 1.0, "step": 0.01, "round": False}),
+                    }
+               }
+    RETURN_TYPES = ("SAMPLER",)
+    CATEGORY = "sampling/custom_sampling/samplers"
+
+    FUNCTION = "get_sampler"
+
+    def get_sampler(self, euler_steps, lcm_steps, ancestral):
+        sampler = comfy.samplers.KSAMPLER(sample_lcm_cycle, extra_options={"euler_steps": euler_steps, "lcm_steps": lcm_steps, "ancestral": ancestral})
+        return (sampler, )
+
 
 NODE_CLASS_MAPPINGS = {
     "LCMScheduler": LCMScheduler,
     "SamplerLCMAlternative": SamplerLCMAlternative,
+    "SamplerLCMCycle": SamplerLCMCycle,
 }
